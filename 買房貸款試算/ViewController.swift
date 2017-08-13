2b7699fc2cf747fac90981bc2a2fb3fbdb5e0cd5
h07870005 <h07870005@yahoo.com.tw>
2017-08-14 00:45:26 +0800

    Initial Commit

---
+++ 買房貸款試算/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  買房貸款試算
+//
+//  Created by Huang Hao on 2017/8/14.
+//  Copyright © 2017年 Huang Hao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
